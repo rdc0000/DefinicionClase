4e653f9285eaf726a674c8977daca80a20c09c17
rdc0000 <rdc.159@hotmail.com>
2019-04-30 17:54:28 -0500

    1

---
+++ DefinicionClase/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefinicionClase
+{
+    public class Persona
+    {
+        #region Propiedades
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+        #endregion
+
+        public void IngresarPersona()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            Edad = Convert.ToInt32(Console.ReadLine());
+        }
+        public void ListarPersonas(List<Persona> user)
+        {
+            foreach (var item in user)
+            {
+                Console.Write($"Nombre: {item.Nombre} Edad: {item.Edad} ");
+            }
+        }
+    }
+}
--- DefinicionClase/Program.cs
+++ DefinicionClase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DefinicionClase
 {
@@ -6,7 +7,56 @@ namespace DefinicionClase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Persona persona = new Persona();
+            Persona per = new Persona();
+            Persona pers = new Persona();
+
+            List<Persona> personas = new List<Persona>();
+            persona.Nombre = "Dario";
+            persona.Edad = 15;
+            Console.WriteLine($"Nombre: {persona.Nombre} Edad: {persona.Edad}");
+            Console.WriteLine("Ingrese su nombre");
+            per.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            per.Edad = Convert.ToInt32(Console.ReadLine());
+            pers.IngresarPersona();
+            Console.WriteLine($"Nombre: {per.Nombre} Edad: {per.Edad}");
+            personas.Add(persona);
+            personas.Add(per);
+            personas.Add(pers);
+            foreach (var item in personas)
+            {
+                Console.Write($"Nombre: {item.Nombre} Edad: {item.Edad} ");
+            }
+
+            Console.WriteLine("Desea ingresar persona? s/n");
+            var res = Console.ReadLine();
+            while (res == "s")
+            {
+                Persona person = new Persona();
+                person.IngresarPersona();
+                personas.Add(person);
+                Console.WriteLine("Desea ingresar persona? s/n");
+                res = Console.ReadLine();
+            }
+            Persona perso = new Persona();
+            perso.ListarPersonas(personas);
+            Console.ReadLine();*/
+            bool result;
+            int num;
+            try
+            {
+                Console.WriteLine("Ingrese la letra");
+                var letra = Console.ReadLine();                
+                result = int.TryParse(letra, out num);
+                Console.WriteLine($"Letra es {result}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha ocurrido un error");
+            }
+            Console.ReadLine();
         }
     }
 }
